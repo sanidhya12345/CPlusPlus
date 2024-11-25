b67e48f9f7efc165c581e80b26bfff420736f5cd
sanidhya12345 <sanidhya.varshney_cs19@gla.ac.in>
2024-11-25 15:31:42 +0530

    fixed password

---
+++ Practice/password.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    while(true){
+        int n;
+        cin>>n;
+        if(n!=1999){
+            cout<<"Wrong"<<endl;
+        }
+        else{
+            cout<<"Correct"<<endl;
+            break;
+        }
+    }
+    return 0;
+}
