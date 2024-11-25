1667674e566fe5c659cf8fb21841b791b672f4dd
sanidhya12345 <sanidhya.varshney_cs19@gla.ac.in>
2024-11-25 14:57:52 +0530

    geometry based pronblem

---
+++ Practice/coordinates.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+    float x,y;
+    cin>>x>>y;
+
+    if(x==0 && y==0){
+        cout<<"Origem";
+    }
+    else if(x==0){
+        cout<<"Eixo Y";
+    }
+    else if(y==0){
+        cout<<"Eixo X";
+    }
+    else{
+        if(x>0 && y>0){
+            cout<<"Q1";
+        }
+        else if(x<0 && y>0){
+            cout<<"Q2";
+        }
+        else if(x<0 && y<0){
+            cout<<"Q3";
+        }
+        else{
+            cout<<"Q4";
+        }
+    }
+    return 0;
+}
