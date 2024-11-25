28fd11a08d3967b236a4ea29943b2ee06e3dec22
sanidhya12345 <sanidhya.varshney_cs19@gla.ac.in>
2024-11-25 11:55:40 +0530

    unordered map in c++

--- Hashing/firstHashing.cpp
+++ Hashing/firstHashing.cpp
@@ -4,6 +4,34 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-	cout<<"hello world";
+	// unordered_map<string,int> map;
+
+	// map["sanidhya"]=10;
+	// map["varshney"]=20;
+
+	// for(auto x:map){
+	// 	cout<<x.first<<x.second;
+	// 	cout<<endl;
+	// }
+
+	int n;
+	cin>>n;
+
+	int arr[n];
+
+	for(int i=0;i<n;i++){
+		cin>>arr[i];
+	}
+
+	unordered_map<int,int> map;
+
+	for(int i:arr){
+		map[i]++;
+	}
+
+	for(auto x:map){
+		cout<<x.first<<" "<<x.second;
+		cout<<endl;
+	}
 	return 0;
 }
