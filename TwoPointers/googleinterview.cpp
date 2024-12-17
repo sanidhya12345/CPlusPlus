aad7ce1a947fcca3636bb46478d92ad66f0fd16d
Sanidhya Varshney <sanidhya.varshney_cs19@gla.ac.in>
2024-12-17 20:25:23 +0530

    Update googleinterview.cpp

--- TwoPointers/googleinterview.cpp
+++ TwoPointers/googleinterview.cpp
@@ -1,5 +1,4 @@
 #include<bits/stdc++.h>
-#include<set>
 #define ll long long
 using namespace std;
 
@@ -7,34 +6,39 @@ int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
     
-    string s;
+    string s, maxSubstr;
     cin >> s;
     int k;
     cin >> k;
 
-    set<char> st; 
-    ll diff = 0;  
-    ll p = 0;     
-    
-    for (int i=0,j = 0; j < s.length(); j++) {
-        st.insert(s[j]);
-        
-        // Update the difference using the largest and smallest elements in the set
-        diff = *st.rbegin() - *st.begin(); 
+    map<char, int> mp; // Sorted map to maintain character frequencies
+    ll diff = 0, p = 0;
+    int start = 0;
 
-        while (diff > k) { // If the difference exceeds k, shrink the window from the left
-            st.erase(s[i]); // Remove the character at the left pointer
+    for (int i = 0, j = 0; j < s.length(); j++) {
+        mp[s[j]]++;
+
+        // Calculate the difference between max and min characters
+        diff = mp.rbegin()->first - mp.begin()->first;
+
+        while (diff > k) {
+            mp[s[i]]--;
+            if (mp[s[i]] == 0) 
+                mp.erase(s[i]);
             i++;
-            if (!st.empty()) { 
-                diff = *st.rbegin() - *st.begin();
-            } else {
-                diff = 0;
-            }
+            diff = !mp.empty() ? mp.rbegin()->first - mp.begin()->first : 0;
         }
-        ll length = j - i + 1; 
-        p = max(p, length);
+
+        ll length = j - i + 1;
+        if (length > p) {
+            p = length;
+            start = i;
+        }
     }
     
+    maxSubstr = s.substr(start, p);
     cout << p << endl;
+    cout << maxSubstr << endl;
+
     return 0;
 }
